db659f96508a5e390172ffaa153225f0eca108b4
SaiKUndurthi <sai.undurthi@gmail.com>
2018-09-19 12:53:57 -0700

    Plot hardship_index vs per_capita_income.

--- Lab 4/analyzeChicagoHardshipIndex.py
+++ Lab 4/analyzeChicagoHardshipIndex.py
@@ -2,8 +2,14 @@ import testDB2Connection as connectionObject;
 import sqlalchemy
 from sqlalchemy import *
 import config;
+import matplotlib.pyplot as plt;
+import seaborn as sns;
 
 chicago_socioeconomic_data = connectionObject.pandas.read_csv('https://data.cityofchicago.org/resource/jcxq-k9xf.csv');
-print(chicago_socioeconomic_data);
 db2 = sqlalchemy.create_engine(config.ibm_db_sa);
-chicago_socioeconomic_data.to_sql('chicago_socioeconomic_data',con=db2,if_exists='fail');
+income_vs_hardship_query = "SELECT per_capita_income_, hardship_index FROM chicago_socioeconomic_data";
+income_vs_hardship = connectionObject.pandas.read_sql(income_vs_hardship_query, connectionObject.pconn);
+print(income_vs_hardship.dtypes);
+plot = sns.jointplot(x="PER_CAPITA_INCOME_", y="HARDSHIP_INDEX", data=income_vs_hardship)
+plt.show();
+connectionObject.ibm_db.close(connectionObject.conn)
